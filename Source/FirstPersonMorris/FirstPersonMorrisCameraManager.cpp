6924d761807526c9860044e7cf3690abc0c88465
Dakota <dskotam45@gmail.com>
2025-07-02 10:36:08 -0500

    Initial Commit

---
+++ Source/FirstPersonMorris/FirstPersonMorrisCameraManager.cpp
@@ -0,0 +1,11 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "FirstPersonMorrisCameraManager.h"
+
+AFirstPersonMorrisCameraManager::AFirstPersonMorrisCameraManager()
+{
+	// set the min/max pitch
+	ViewPitchMin = -70.0f;
+	ViewPitchMax = 80.0f;
+}
